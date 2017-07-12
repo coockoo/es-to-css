a10e4b4a2629e7cdde85467c980d6946ab0cc791
Alexandr Bondarchuk <alex.coockoo@gmail.com>
2017-07-12 23:08:34 +0300

    Add initial tests with mocha

--- test/index.js
+++ test/index.js
@@ -1,14 +1,66 @@
+const assert = require('assert')
+
 const toCSS = require('../src')
 
-const obj = {
-	'#my-id-element': {
-		'background-color': 'red'
-	},
-	'.my-class-element': {
-		'font-family': 'arial'
-	}
-};
-const actual = toCSS(obj)
-const expected = '#my-id-element { background-color: red; }\n.my-class-element { font-family: arial; }'
+describe('toCSS', () => {
+	it('should pass simple object => style test', () => {
+		runTest({
+			obj: {
+				'#my-id-element': {
+					'background-color': 'red'
+				},
+				'.my-class-element': {
+					'font-family': 'arial'
+				}
+			},
+			css: '#my-id-element { background-color: red; }\n.my-class-element { font-family: arial; }',
+		});
+	});
+	it('should work properly with nested styles', () => {
+		runTest({
+			obj: {
+				'#my-id-element': {
+					'background-color': 'red',
+					'.my-class-element': {
+						'font-family': 'arial'
+					}
+				}
+			},
+			css: '#my-id-element { background-color: red; }\n#my-id-element .my-class-element { font-family: arial; }',
+		});
+	});
+	it('should work properly with & and pseudo-classes', () => {
+		runTest({
+			obj: {
+				'#my-id-element': {
+					'background-color': 'red',
+					'&:hover': {
+						'background-color': 'black'
+					}
+				}
+			},
+			css: '#my-id-element { background-color: red; }\n#my-id-element:hover { background-color: black; }',
+		});
+	});
+	it('should work with option blockSeparator', () => {
+		runTest({
+			obj: {
+				'#my-id-element': {
+					'background-color': 'red'
+				},
+				'.my-class-element': {
+					'font-family': 'arial'
+				}
+			},
+			css: '#my-id-element { background-color: red; } .my-class-element { font-family: arial; }',
+			opts: {
+				blockSeparator: ' '
+			}
+		});
+	});
+});
 
-console.log('Should be truthfully', actual === expected)
+function runTest ({ obj, css, opts = {} }) {
+	const actual = toCSS(obj, opts);
+	assert.equal(actual, css);
+}
